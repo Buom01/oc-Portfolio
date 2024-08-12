8a6a2ad5780904b394b030f910b19ae558edb8c1
Bastien ADAM <bastien.adam.buom01@gmail.com>
2024-08-12 19:25:49 +0200

    Add profile picture + minor tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 import { Message } from "@/lib/@GrangerHub/react-webkit/components/message";
 import Nav from "@/lib/@GrangerHub/react-webkit/components/nav";
+import Picture from "@/lib/@GrangerHub/react-webkit/components/picture";
 import {Title, Container} from "@/lib/@GrangerHub/react-webkit/components/utils";
 import NextLink  from "next/link";
+import Image from 'next/image';
+
+import picture from './assets/sebastian-adam.png';
+
 
 export default function Home() {
   return (
     <main>
-      <Title level={1}>Bastien ADAM</Title>
+      <Title level={1}>Sebastian ADAM</Title>
       <Title level={2}>Développeur informatique</Title>
 
       <Nav
@@ -21,14 +26,20 @@ export default function Home() {
         />
 
       <Container id="à-propos">
-        <Message title="À propos">
+        <Message title="À propos" TitleTag="h3">
+          <Picture
+              alt="Sebastian ADAM - Développeur informatique Web et C++" right
+              placeholderComponent={Image} {...picture} placeholder="blur"
+            />
           Ayant terminé la certification de l&apos;École 42 Paris, je finalise désormais le diplôme RNCP niveau 7 <i>Expert en architecture informatique</i> afin de continuer sur un doctorat.<br/>
           J&apos;ai de plus certifié cette année mes compétences pré-acquises en développement d&apos;application web par la formation OpenClassrooms <i>Développeur Web</i>.<br/>
           <br/>
-          Considérant mes expériences professionnelles et mon implication dans des collectifs annexes, ma curiosité m&apos;amène à la recherche de missions CDD/CDI consécutives.<br/>
+          Considérant mes expériences professionnelles et mon implication dans des collectifs annexes, ma curiosité m&apos;amène à la recherche de missions CDD/CDI consécutives en entreprise.<br/>
         </Message>
       </Container>
 
+      <Container id="compétences">
+      </Container>
 
     </main>
   );
