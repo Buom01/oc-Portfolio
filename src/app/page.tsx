e3896dc5ec369d2c264c493d8a602f6db23e81f5
Bastien ADAM <bastien.adam.buom01@gmail.com>
2024-08-16 04:55:01 +0200

    Add contact section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import { Message } from "@/lib/@GrangerHub/react-webkit/components/message";
 import Nav from "@/lib/@GrangerHub/react-webkit/components/nav";
-import Picture from "@/lib/@GrangerHub/react-webkit/components/picture";
 import {Title, Container} from "@/lib/@GrangerHub/react-webkit/components/utils";
+import Picture from "@/lib/@GrangerHub/react-webkit/components/picture";
+import Application, { Applications } from "@/lib/@GrangerHub/react-webkit/components/application";
 import NextLink  from "next/link";
 import Image from 'next/image';
 
 import picture from './assets/sebastian-adam.png';
+import { mdiGmail, mdiGithub, mdiLinkedin, mdiPhone } from '@mdi/js';
+import malt from './assets/malt.svg'
 
 
 export default function Home() {
@@ -17,10 +20,10 @@ export default function Home() {
       <Nav
           items={[
             {text: "À propos", href: "/#à-propos", inner: true, Component: NextLink},
+            {text: "Contacter", href: "/#contacter", inner: true, Component: NextLink},
             {text: "Compétences", href: "/#compétences", inner: true, Component: NextLink},
             {text: "Diplômes", href: "/#diplômes", inner: true, Component: NextLink},
             {text: "Projets", href: "/#projets", inner: true, Component: NextLink},
-            {text: "Contacter", href: "/#contacter", inner: true, Component: NextLink},
             {text: "Curriculum Vitæ", href: "https://bastien-adam.fr/Bastien%20ADAM%20-%20CV.pdf"},
           ]}
         />
@@ -29,13 +32,23 @@ export default function Home() {
         <Message title="À propos" TitleTag="h3">
           <Picture
               alt="Sebastian ADAM - Développeur informatique Web et C++" right
-              placeholderComponent={Image} {...picture} placeholder="blur"
+              Component={Image} {...picture} placeholder="blur"
             />
           Ayant terminé la certification de l&apos;École 42 Paris, je finalise désormais le diplôme RNCP niveau 7 <i>Expert en architecture informatique</i> afin de continuer sur un doctorat.<br/>
           J&apos;ai de plus certifié cette année mes compétences pré-acquises en développement d&apos;application web par la formation OpenClassrooms <i>Développeur Web</i>.<br/>
           <br/>
           Considérant mes expériences professionnelles et mon implication dans des collectifs annexes, ma curiosité m&apos;amène à la recherche de missions CDD/CDI consécutives en entreprise.<br/>
         </Message>
+      </Container>
+
+      <Container id="contacter">
+        <Applications>
+          <Application href="/" title="Courriel" color="#d44638" path={mdiGmail} label="bastien.adam.buom01@gmail.com"/>
+          <Application href="/" title="Téléphone" color="#25D366" path={mdiPhone}/>
+          <Application href="/" title="Github" color="#aaa" path={mdiGithub}/>
+          <Application href="/" title="LinkedIn" color="#0e76a8" path={mdiLinkedin}/>
+          <Application href="/" title="Malt" color="#FC5656" vector={malt}/>
+        </Applications>
       </Container>
 
       <Container id="compétences">
