a875c3af403f1a22ca5a87b44b3058aa13bd4528
Bastien ADAM <bastien.adam.buom01@gmail.com>
2024-08-16 04:58:23 +0200

    [WIP] Add competences + graduations + projects, with mockup data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import Nav from "@/lib/@GrangerHub/react-webkit/components/nav";
 import {Title, Container} from "@/lib/@GrangerHub/react-webkit/components/utils";
 import Picture from "@/lib/@GrangerHub/react-webkit/components/picture";
 import Application, { Applications } from "@/lib/@GrangerHub/react-webkit/components/application";
+import { Human } from "@/lib/@GrangerHub/react-webkit/components/block";
+import {Basic, Simple} from "@/lib/@GrangerHub/react-webkit/components/block/layouts";
+import {HumanList, ListContainer, ListGrid} from "@/lib/@GrangerHub/react-webkit/components/list";
 import NextLink  from "next/link";
 import Image from 'next/image';
 
@@ -52,8 +55,116 @@ export default function Home() {
       </Container>
 
       <Container id="compétences">
+        <Title level={3}>Compétences techniques</Title>
+        <Basic Component={Human} width="100%" height="initial">
+          <ListGrid>
+            <ListContainer title="Languages">
+              <HumanList>
+                <>Hello</>
+                <>World</>
+              </HumanList>
+            </ListContainer>
+            <ListContainer title="Frameworks">
+              <HumanList>
+                <>Hello</>
+                <>World</>
+                <>World</>
+              </HumanList>
+            </ListContainer>
+            <ListContainer title="DevOps">
+              <HumanList>
+                <>Hello</>
+                <>World</>
+              </HumanList>
+            </ListContainer>
+            <ListContainer title="Cloud">
+              <HumanList>
+                <>Hello</>
+                <>World</>
+              </HumanList>
+            </ListContainer>
+            <ListContainer title="Bases de données">
+              <HumanList>
+                <>Hello</>
+                <>World</>
+              </HumanList>
+            </ListContainer>
+            <ListContainer title="Logiciels">
+              <HumanList>
+                <>Hello</>
+                <>World</>
+              </HumanList>
+            </ListContainer>
+          </ListGrid>
+        </Basic>
       </Container>
 
+      <Container id="diplômes">
+        <Title level={3}>Formations et diplômes</Title>
+        <table>
+          <thead>
+            <tr>
+              <th className="spoiler"></th>
+              <th>Organisme</th>
+              <th>Diplôme</th>
+              <th>Niveau</th>
+              <th>Status</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th></th>
+              <th>OpenClassrooms</th>
+              <th></th>
+              <th>Bac +3</th>
+              <th>En validation</th>
+            </tr>
+            <tr>
+            <th></th>
+              <th>École 42</th>
+              <th>Architecte du numérique</th>
+              <th>Bac +5</th>
+              <th>Finalisé</th>
+            </tr>
+            <tr>
+              <th></th>
+              <th>École 42</th>
+              <th></th>
+              <th>RNCP niveau 7</th>
+              <th>En finalisation</th>
+            </tr>
+            <tr>
+              <th></th>
+              <th>Public</th>
+              <th>CPGE TSI</th>
+              <th>Bac +2</th>
+              <th>Finalisé</th>
+            </tr>
+          </tbody>
+        </table>
+      </Container>
+
+      <Container id="projets">
+        <Title level={3}>Projets</Title>
+        <Simple title="WebServ" TitleComponent="h4" Component={Human} width="100%" height="initial">
+          <Title level={5}>Contexte</Title>
+          <p>[Une brève description du projet, y compris les objectifs commerciaux ou techniques, l'industrie, et les utilisateurs finaux.]</p>
+          <Title level={5}>Description</Title>
+          <p>
+            Serveur web C++ à la Nginx, réalisé sous forme de middleware, from scratch.<br/>
+            Avec lecture / écriture de fichiers, et CGI. Hautement configurable, code facilement extensible.<br/>
+            Benchmarké et validé par le testeur facultatif fourni.
+          </p>
+          <Title level={5}>Rôle et responsabilités</Title>
+          <p>[La performance de nos APIs]</p>
+          <Title level={5}>Environement</Title>
+          <Title level={5}>Résultats</Title>
+          <Title level={5}>Réalisations</Title>
+          <Title level={5}>Annexes</Title>
+          [code sources / média]
+        </Simple>
+      </Container>
     </main>
   );
 }
